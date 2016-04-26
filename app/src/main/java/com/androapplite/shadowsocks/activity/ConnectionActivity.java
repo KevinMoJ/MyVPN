ce08cd37994fdc81599a7243c860d0421958f271
jim <reachjim@gmail.com>
2016-04-26 19:51:57 +0800

    theme用actionbar

--- app/src/main/java/com/androapplite/shadowsocks/activity/ConnectionActivity.java
+++ app/src/main/java/com/androapplite/shadowsocks/activity/ConnectionActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 import com.androapplite.shadowsocks.R;
 import com.androapplite.shadowsocks.broadcast.Action;
@@ -15,11 +17,23 @@ public class ConnectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection);
+        initToobar();
+    }
+
+    private void initToobar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(Action.CONNECTION_ACTIVITY_SHOW));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_connection_activity, menu);
+        return true;
     }
 }
