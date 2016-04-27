253b7c64346ec4992a847f5eee6a3f373f10737a
jim <reachjim@gmail.com>
2016-04-27 10:29:44 +0800

    分享图标变白色

---
+++ app/src/main/java/com/androapplite/shadowsocks/activity/NewUserGuideActivity.java
@@ -0,0 +1,44 @@
+package com.androapplite.shadowsocks.activity;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.androapplite.shadowsocks.R;
+
+public class NewUserGuideActivity extends AppCompatActivity {
+    private ViewPager mWizardPager;
+    private NewUserGuidePagerAdapter mNewUserGuidePagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_user_guide);
+        mWizardPager = (ViewPager)findViewById(R.id.wizard_pager);
+        mNewUserGuidePagerAdapter = new NewUserGuidePagerAdapter(getSupportFragmentManager());
+        initWizardPager();
+
+    }
+    private void initWizardPager(){
+        mWizardPager.setAdapter(mNewUserGuidePagerAdapter);
+    }
+
+    private static class NewUserGuidePagerAdapter extends FragmentPagerAdapter{
+        public NewUserGuidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+    }
+}
