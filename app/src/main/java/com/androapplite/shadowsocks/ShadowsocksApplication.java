c6be2ddde63342d073443c48b2757b4b6d09c83f
jim <reachjim@gmail.com>
2016-05-05 11:40:18 +0800

    增加strickMode

--- app/src/main/java/com/androapplite/shadowsocks/ShadowsocksApplication.java
+++ app/src/main/java/com/androapplite/shadowsocks/ShadowsocksApplication.java
@@ -1,6 +1,7 @@
 package com.androapplite.shadowsocks;
 
 import android.app.Application;
+import android.os.StrictMode;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
@@ -39,6 +40,17 @@ public class ShadowsocksApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Fabric.with(this, new Crashlytics());
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build());
+        }
     }
 
     public static final void debug(@NonNull String tag, @NonNull String msg){
