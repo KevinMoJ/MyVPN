a836101285dd469b9ab474c18f4cfca439e57011
jim <reachjim@gmail.com>
2016-05-10 19:46:03 +0800

    增加last_reset_month变量

--- app/src/main/java/yyf/shadowsocks/preferences/SharedPreferenceKey.java
+++ app/src/main/java/yyf/shadowsocks/preferences/SharedPreferenceKey.java
@@ -6,4 +6,5 @@ package yyf.shadowsocks.preferences;
 public final class SharedPreferenceKey {
     public static final String TX_TOTAL = "TX_TOTAL";
     public static final String RX_TOTAL = "RX_TOTAL";
+    public static final String LAST_RESET_MONTH = "LAST_RESET_MONTH";
 }
