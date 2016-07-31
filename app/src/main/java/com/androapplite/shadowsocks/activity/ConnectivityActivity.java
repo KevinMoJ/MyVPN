6bfc78bbf421f849f1177755826668095b39a1c4
jim <reachjim@gmail.com>
2016-07-31 12:29:41 +0800

    显示抽屉里的内容

--- app/src/main/java/com/androapplite/shadowsocks/activity/ConnectivityActivity.java
+++ app/src/main/java/com/androapplite/shadowsocks/activity/ConnectivityActivity.java
@@ -75,24 +75,27 @@ public class ConnectivityActivity extends BaseShadowsocksActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
+//        if (id == R.id.nav_camera) {
+//            // Handle the camera action
+//        } else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_manage) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
+
 
     @Override
     public void onClickConnectionButton() {
