d05cef36910cc08409c492fd9d4c35bc023f6227
jim <reachjim@gmail.com>
2016-07-31 12:56:13 +0800

    创建connectivity fragment

---
+++ app/src/main/java/com/androapplite/shadowsocks/fragment/ConnectivityFragment.java
@@ -0,0 +1,30 @@
+package com.androapplite.shadowsocks.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.androapplite.shadowsocks.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ConnectivityFragment extends Fragment {
+
+
+    public ConnectivityFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_connectivity, container, false);
+    }
+
+}
