e984e2282b33eacd1b08712a5b13088b50ed3ba9
jim <reachjim@gmail.com>
2016-08-01 16:55:48 +0800

    跳过connectivity fragment

--- app/src/main/java/com/androapplite/shadowsocks/fragment/ConnectivityFragment.java
+++ app/src/main/java/com/androapplite/shadowsocks/fragment/ConnectivityFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.androapplite.shadowsocks.R;
 
@@ -24,7 +26,9 @@ public class ConnectivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_connectivity, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_connectivity, container, false);
+
+        return rootView;
     }
 
 }
