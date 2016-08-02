4f91710c02499136c4ded8a276b7a47983bd5414
jim <reachjim@gmail.com>
2016-08-02 11:34:14 +0800

    实验loading颜色

--- app/src/main/java/com/androapplite/shadowsocks/fragment/ConnectivityFragment.java
+++ app/src/main/java/com/androapplite/shadowsocks/fragment/ConnectivityFragment.java
@@ -1,13 +1,17 @@
 package com.androapplite.shadowsocks.fragment;
 
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.androapplite.shadowsocks.R;
 
@@ -27,7 +31,56 @@ public class ConnectivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_connectivity, container, false);
+        final ProgressBar progressBar = (ProgressBar)rootView.findViewById(R.id.progressBar);
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(getActivity(), R.animator.connecting);
+        set.setTarget(progressBar);
+        set.getChildAnimations().get(0).addListener(new AnimatorListenerAdapter() {
+            private int mCount = 0;
 
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                Log.d("end", "");
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+                if(mCount == 0){
+                    progressBar.setProgressDrawable(getResources().getDrawable(R.drawable.connecting_phase_2));
+                }else if(mCount == 1){
+                    progressBar.setProgressDrawable(getResources().getDrawable(R.drawable.connecting_phase_3));
+                }
+                mCount++;
+            }
+        });
+//        set.addListener(new AnimatorListenerAdapter() {
+//            private int mCount = 0;
+//
+//            @Override
+//            public void onAnimationEnd(Animator animation) {
+//                Log.d("end", mCount + "");
+//            }
+//
+//            @Override
+//            public void onAnimationRepeat(Animator animation) {
+//                Log.d("repeat", mCount + "");
+//            }
+//        });
+        set.start();
+
+//        Animator animator = AnimatorInflater.loadAnimator(getActivity(), R.animator.connecting);
+//        animator.setTarget(progressBar);
+//        animator.start();
+//        animator.addListener(new AnimatorListenerAdapter() {
+//            @Override
+//            public void onAnimationRepeat(Animator animation) {
+//                Log.d("repeat", "-");
+//            }
+//
+//            @Override
+//            public void onAnimationEnd(Animator animation) {
+//                Log.d("end", "-");
+//            }
+//        });
         return rootView;
     }
 
