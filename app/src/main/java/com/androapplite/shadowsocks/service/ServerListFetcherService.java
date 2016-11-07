3bc32f90363b18a3cb6cfbace90f2c94d0ad1006
jim <reachjim@gmail.com>
2016-11-07 19:13:22 +0800

    增加访问服务器列表的及时统计

--- app/src/main/java/com/androapplite/shadowsocks/service/ServerListFetcherService.java
+++ app/src/main/java/com/androapplite/shadowsocks/service/ServerListFetcherService.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.util.Log;
 
+import com.androapplite.shadowsocks.GAHelper;
+import com.androapplite.shadowsocks.ShadowsocksApplication;
 import com.androapplite.shadowsocks.preference.DefaultSharedPrefeencesUtil;
 import com.androapplite.shadowsocks.preference.SharedPreferenceKey;
 
@@ -34,19 +36,25 @@ public class ServerListFetcherService extends IntentService {
         SharedPreferences sp = DefaultSharedPrefeencesUtil.getDefaultSharedPreferences(this);
         sp.edit().remove(SharedPreferenceKey.SERVER_LIST).apply();
         OkHttpClient client = new OkHttpClient();
+
         String url = "http://192.168.31.29/vpn/server_list.json";
         Request request = new Request.Builder()
                 .url(url)
                 .build();
 
 
+        long t1 = System.currentTimeMillis();
         try {
             Response response = client.newCall(request).execute();
+            long t2 = System.currentTimeMillis();
+            GAHelper.sendTimingEvent(this, "访问服务器列表", "成功", t2-t1);
             String jsonString = response.body().string();
 //            SharedPreferences sp = DefaultSharedPrefeencesUtil.getDefaultSharedPreferences(this);
             sp.edit().putString(SharedPreferenceKey.SERVER_LIST, jsonString).apply();
         } catch (IOException e) {
-            e.printStackTrace();
+            long t2 = System.currentTimeMillis();
+            GAHelper.sendTimingEvent(this, "访问服务器列表", "失败", t2-t1);
+            ShadowsocksApplication.handleException(e);
         }
     }
 
