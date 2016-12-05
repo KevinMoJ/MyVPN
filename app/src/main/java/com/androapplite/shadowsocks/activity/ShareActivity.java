9cd85e224d11aec2c18a215191bf40b0f502edf2
jim <reachjim@gmail.com>
2016-12-05 17:41:52 +0800

    facebook share

--- app/src/main/java/com/androapplite/shadowsocks/activity/ShareActivity.java
+++ app/src/main/java/com/androapplite/shadowsocks/activity/ShareActivity.java
@@ -1,9 +1,13 @@
 package com.androapplite.shadowsocks.activity;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.androapplite.shadowsocks.R;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareDialog;
 
 public class ShareActivity extends AppCompatActivity {
 
@@ -11,5 +15,14 @@ public class ShareActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
+    }
+
+    public void shareByFacebook(View view){
+        ShareLinkContent content = new ShareLinkContent.Builder()
+                .setContentUrl(Uri.parse("https://play.google.com/store/apps/details?id=com.androapplite.shadowsocks&referrer=utm_source%3Dclient%26utm_medium%3Dfacebook\n"))
+                .build();
+
+        ShareDialog shareDialog = new ShareDialog(this);
+        shareDialog.show(content);
     }
 }
