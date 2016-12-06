c906df6baeaea72d937b17e8cdbbd50260054be7
jim <reachjim@gmail.com>
2016-12-06 20:09:35 +0800

    通知打开关闭上移到activity处理

--- app/src/main/java/com/androapplite/shadowsocks/activity/SettingsActivity.java
+++ app/src/main/java/com/androapplite/shadowsocks/activity/SettingsActivity.java
@@ -1,13 +1,19 @@
 package com.androapplite.shadowsocks.activity;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.androapplite.shadowsocks.R;
+import com.androapplite.shadowsocks.ShadowsocksApplication;
 import com.androapplite.shadowsocks.fragment.SettingsFragment;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements SettingsFragment.OnSettingsActionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,4 +35,27 @@ public class SettingsActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void about() {
+        PackageManager packageManager = getPackageManager();
+        String packageName = getPackageName();
+        try {
+            PackageInfo packageInfo = packageManager.getPackageInfo(packageName, 0);
+            String version = packageInfo.versionName;
+
+            String appName = getResources().getString(R.string.app_name);
+
+            new AlertDialog.Builder(this)
+                    .setTitle(R.string.about)
+                    .setMessage(appName + " (" + version + ")")
+                    .show();
+        } catch (PackageManager.NameNotFoundException e) {
+            ShadowsocksApplication.handleException(e);
+        }
+    }
+
+    @Override
+    public void setNotificationEnable(boolean enable) {
+        Toast.makeText(this, "notification " + enable, Toast.LENGTH_SHORT).show();
+    }
 }
--- app/src/main/java/com/androapplite/shadowsocks/fragment/SettingsFragment.java
+++ app/src/main/java/com/androapplite/shadowsocks/fragment/SettingsFragment.java
@@ -1,6 +1,7 @@
 package com.androapplite.shadowsocks.fragment;
 
 
+import android.app.Activity;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -9,11 +10,7 @@ import android.preference.PreferenceFragment;
 import android.preference.SwitchPreference;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.androapplite.shadowsocks.R;
@@ -21,15 +18,13 @@ import com.androapplite.shadowsocks.ShadowsocksApplication;
 import com.androapplite.shadowsocks.preference.DefaultSharedPrefeencesUtil;
 import com.androapplite.shadowsocks.preference.SharedPreferenceKey;
 
-import static java.security.AccessController.getContext;
-
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SettingsFragment extends PreferenceFragment implements Preference.OnPreferenceClickListener,
         Preference.OnPreferenceChangeListener
 {
-
+    private OnSettingsActionListener mListener;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -57,35 +52,42 @@ public class SettingsFragment extends PreferenceFragment implements Preference.O
 
     @Override
     public boolean onPreferenceClick(Preference preference) {
-        about();
-        return true;
-    }
-
-    private void about(){
-        PackageManager packageManager = getActivity().getPackageManager();
-        String packageName = getActivity().getPackageName();
-        try {
-            PackageInfo packageInfo = packageManager.getPackageInfo(packageName, 0);
-            String version = packageInfo.versionName;
-
-            String appName = getResources().getString(R.string.app_name);
-
-            new AlertDialog.Builder(getActivity())
-                    .setTitle(R.string.about)
-                    .setMessage(appName + " (" + version + ")")
-                    .show();
-        } catch (PackageManager.NameNotFoundException e) {
-            ShadowsocksApplication.handleException(e);
+        if(mListener != null){
+            mListener.about();
         }
+        return true;
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         String key = preference.getKey();
         if(key.equals(SharedPreferenceKey.NOTIFICATION)){
-            Toast.makeText(getActivity(), "notification " + newValue, Toast.LENGTH_SHORT).show();
+            if(mListener != null){
+                mListener.setNotificationEnable((Boolean)newValue);
+            }
             return true;
         }
         return false;
+    }
+
+    public interface OnSettingsActionListener {
+        void about();
+        void setNotificationEnable(boolean enable);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        if(activity instanceof OnSettingsActionListener){
+            mListener = (OnSettingsActionListener)activity;
+        }else{
+            throw new ClassCastException(activity.getLocalClassName() + " must implement " + OnSettingsActionListener.class.getSimpleName());
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
     }
 }
