47616612e9ff27f1ea401f65f049ff9223d1d15e
jim <reachjim@gmail.com>
2016-12-09 00:26:30 +0800

    动画

--- app/src/main/java/com/androapplite/shadowsocks/fragment/ConnectFragment.java
+++ app/src/main/java/com/androapplite/shadowsocks/fragment/ConnectFragment.java
@@ -1,19 +1,23 @@
 package com.androapplite.shadowsocks.fragment;
 
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.androapplite.shadowsocks.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ConnectFragment extends Fragment {
-
+public class ConnectFragment extends Fragment implements View.OnClickListener{
+    ImageView mJaguarImageView;
+    ImageView mJaguarAnimationImageView;
 
     public ConnectFragment() {
         // Required empty public constructor
@@ -25,8 +29,15 @@ public class ConnectFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_connect, container, false);
-
+        view.findViewById(R.id.connect_button).setOnClickListener(this);
+        mJaguarImageView = (ImageView)view.findViewById(R.id.jaguar_image_view);
+        mJaguarAnimationImageView = (ImageView)view.findViewById(R.id.jaguar_animation_image_view);
         return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        AnimationDrawable animationDrawable = (AnimationDrawable)mJaguarAnimationImageView.getDrawable();
+        animationDrawable.start();
+    }
 }
