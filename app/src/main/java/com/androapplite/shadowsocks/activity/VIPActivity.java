a3ee6f9ea8eb3ce13eaa8533b8cf094e6f91779b
jim <reachjim@gmail.com>
2017-01-14 17:43:24 +0800

    增加支付的代码

--- app/src/main/java/com/androapplite/shadowsocks/activity/VIPActivity.java
+++ app/src/main/java/com/androapplite/shadowsocks/activity/VIPActivity.java
@@ -1,15 +1,174 @@
 package com.androapplite.shadowsocks.activity;
 
+import android.content.IntentFilter;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.androapplite.shadowsocks.R;
+import com.androapplite.shadowsocks.util.IabBroadcastReceiver;
+import com.androapplite.shadowsocks.util.IabHelper;
+import com.androapplite.shadowsocks.util.IabResult;
+import com.androapplite.shadowsocks.util.Inventory;
+import com.androapplite.shadowsocks.util.Purchase;
 
-public class VIPActivity extends AppCompatActivity {
+public class VIPActivity extends AppCompatActivity implements IabBroadcastReceiver.IabBroadcastListener {
+    private IabHelper mHelper;
+    private IabBroadcastReceiver mBroadcastReceiver;
+    private static final String TAG = "VIPActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vip);
+
+        String base64EncodedPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAr/HCpmwW+jdTIGpJ1AAlQizVh1C1Y4wNXnb3ncidfpwfQKWgrOdENY6TN083JCSnncV9ttOSZr+fW7b/FJcgzmbkY7vF9MZGhkraNBU1iPYb8ALxgQugV1sihtYsrmtjWr8Tw6lIVa4BBYz7pxLZr5n6LMKEEEAWPzwNqHW/kiBEULqqNlp0wXUlDgcCaFIBgjWfumrkZAuAFWwJzES/vKgwT4zV2PHAzEhyc8GO4OhzHVTgMPYluWbJsac1uQlDB7Re8KdyzV36ImBnQVZ47gqRwG/gaQEuoReB9f7D0trjoQoyRSQsv2J9gskZ7mDTweaHmv2ARFaUZi0yWUsH4QIDAQAB";
+
+        // compute your public key and store it in base64EncodedPublicKey
+        mHelper = new IabHelper(this, base64EncodedPublicKey);
+        mHelper.enableDebugLogging(true);
+        mHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
+            public void onIabSetupFinished(IabResult result) {
+                Log.d(TAG, "Setup finished.");
+
+                if (!result.isSuccess()) {
+                    // Oh noes, there was a problem.
+                    complain("Problem setting up in-app billing: " + result);
+                    return;
+                }
+
+                // Have we been disposed of in the meantime? If so, quit.
+                if (mHelper == null) return;
+
+                // Important: Dynamically register for broadcast messages about updated purchases.
+                // We register the receiver here instead of as a <receiver> in the Manifest
+                // because we always call getPurchases() at startup, so therefore we can ignore
+                // any broadcasts sent while the app isn't running.
+                // Note: registering this listener in an Activity is a bad idea, but is done here
+                // because this is a SAMPLE. Regardless, the receiver must be registered after
+                // IabHelper is setup, but before first call to getPurchases().
+                mBroadcastReceiver = new IabBroadcastReceiver(VIPActivity.this);
+                IntentFilter broadcastFilter = new IntentFilter(IabBroadcastReceiver.ACTION);
+                registerReceiver(mBroadcastReceiver, broadcastFilter);
+
+                // IAB is fully set up. Now, let's get an inventory of stuff we own.
+                Log.d(TAG, "Setup successful. Querying inventory.");
+                try {
+                    mHelper.queryInventoryAsync(mGotInventoryListener);
+                } catch (IabHelper.IabAsyncInProgressException e) {
+                    complain("Error querying inventory. Another async operation in progress.");
+                }
+            }
+        });
+    }
+
+    // Listener that's called when we finish querying the items and subscriptions we own
+    IabHelper.QueryInventoryFinishedListener mGotInventoryListener = new IabHelper.QueryInventoryFinishedListener() {
+        public void onQueryInventoryFinished(IabResult result, Inventory inventory) {
+            Log.d(TAG, "Query inventory finished.");
+
+            // Have we been disposed of in the meantime? If so, quit.
+            if (mHelper == null) return;
+
+            // Is it a failure?
+            if (result.isFailure()) {
+                complain("Failed to query inventory: " + result);
+                return;
+            }
+
+            Log.d(TAG, "Query inventory was successful.");
+
+            /*
+             * Check for items we own. Notice that for each purchase, we check
+             * the developer payload to see if it's correct! See
+             * verifyDeveloperPayload().
+             */
+
+            // Do we have the premium upgrade?
+//            Purchase premiumPurchase = inventory.getPurchase("sku");
+//            mIsPremium = (premiumPurchase != null && verifyDeveloperPayload(premiumPurchase));
+//            Log.d(TAG, "User is " + (mIsPremium ? "PREMIUM" : "NOT PREMIUM"));
+
+            // First find out which subscription is auto renewing
+//            Purchase gasMonthly = inventory.getPurchase(SKU_INFINITE_GAS_MONTHLY);
+//            Purchase gasYearly = inventory.getPurchase(SKU_INFINITE_GAS_YEARLY);
+//            if (gasMonthly != null && gasMonthly.isAutoRenewing()) {
+//                mInfiniteGasSku = SKU_INFINITE_GAS_MONTHLY;
+//                mAutoRenewEnabled = true;
+//            } else if (gasYearly != null && gasYearly.isAutoRenewing()) {
+//                mInfiniteGasSku = SKU_INFINITE_GAS_YEARLY;
+//                mAutoRenewEnabled = true;
+//            } else {
+//                mInfiniteGasSku = "";
+//                mAutoRenewEnabled = false;
+//            }
+//
+//            // The user is subscribed if either subscription exists, even if neither is auto
+//            // renewing
+//            mSubscribedToInfiniteGas = (gasMonthly != null && verifyDeveloperPayload(gasMonthly))
+//                    || (gasYearly != null && verifyDeveloperPayload(gasYearly));
+//            Log.d(TAG, "User " + (mSubscribedToInfiniteGas ? "HAS" : "DOES NOT HAVE")
+//                    + " infinite gas subscription.");
+//            if (mSubscribedToInfiniteGas) mTank = TANK_MAX;
+//
+//            // Check for gas delivery -- if we own gas, we should fill up the tank immediately
+//            Purchase gasPurchase = inventory.getPurchase(SKU_GAS);
+//            if (gasPurchase != null && verifyDeveloperPayload(gasPurchase)) {
+//                Log.d(TAG, "We have gas. Consuming it.");
+//                try {
+//                    mHelper.consumeAsync(inventory.getPurchase(SKU_GAS), mConsumeFinishedListener);
+//                } catch (IabAsyncInProgressException e) {
+//                    complain("Error consuming gas. Another async operation in progress.");
+//                }
+//                return;
+//            }
+//
+//            updateUi();
+//            setWaitScreen(false);
+            Log.d(TAG, "Initial inventory query finished; enabling main UI.");
+        }
+    };
+
+    void complain(String message) {
+        Log.e(TAG, "**** TrivialDrive Error: " + message);
+        alert("Error: " + message);
+    }
+
+
+    void alert(String message) {
+        AlertDialog.Builder bld = new AlertDialog.Builder(this);
+        bld.setMessage(message);
+        bld.setNeutralButton("OK", null);
+        Log.d(TAG, "Showing alert dialog: " + message);
+        bld.create().show();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        // very important:
+        if (mBroadcastReceiver != null) {
+            unregisterReceiver(mBroadcastReceiver);
+        }
+
+        // very important:
+        Log.d(TAG, "Destroying helper.");
+        if (mHelper != null) {
+            mHelper.disposeWhenFinished();
+            mHelper = null;
+        }
+    }
+
+    @Override
+    public void receivedBroadcast() {
+        // Received a broadcast notification that the inventory of items has changed
+        Log.d(TAG, "Received broadcast notification. Querying inventory.");
+        try {
+            mHelper.queryInventoryAsync(mGotInventoryListener);
+        } catch (IabHelper.IabAsyncInProgressException e) {
+            complain("Error querying inventory. Another async operation in progress.");
+        }
     }
 }
