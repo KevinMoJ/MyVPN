0d8c1cc6bc5a21005fd9a5f6aad376a674fe25d1
jim <reachjim@gmail.com>
2017-01-20 20:36:46 +0800

    视频事件

--- app/src/main/java/com/androapplite/shadowsocks/activity/WatchVideoDialogActivity.java
+++ app/src/main/java/com/androapplite/shadowsocks/activity/WatchVideoDialogActivity.java
@@ -2,7 +2,9 @@ package com.androapplite.shadowsocks.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Toast;
 
 import com.androapplite.shadowsocks.R;
 
@@ -12,6 +14,12 @@ public class WatchVideoDialogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_watch_video_dialog);
-        setFinishOnTouchOutside(false);
+//        setFinishOnTouchOutside(false);
+    }
+
+    public void watchVideo(View v){
+        finish();
+        //视频广告
+        Toast.makeText(this, "视频广告", Toast.LENGTH_SHORT).show();
     }
 }
