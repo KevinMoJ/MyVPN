706338886a6929908076c11db2b693ed98fb2c26
jim <reachjim@gmail.com>
2017-02-08 23:51:14 +0800

    正确的显示签到

--- app/src/main/java/com/androapplite/shadowsocks/activity/CheckInActivity.java
+++ app/src/main/java/com/androapplite/shadowsocks/activity/CheckInActivity.java
@@ -1,13 +1,17 @@
 package com.androapplite.shadowsocks.activity;
 
 import android.app.AlarmManager;
+import android.content.SharedPreferences;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.androapplite.shadowsocks.R;
+import com.androapplite.shadowsocks.preference.DefaultSharedPrefeencesUtil;
+import com.androapplite.shadowsocks.preference.SharedPreferenceKey;
 
 import java.util.ArrayList;
 
@@ -25,6 +29,14 @@ public class CheckInActivity extends AppCompatActivity {
             mSmileImages.add((ImageView)linearLayout.getChildAt(1));
         }
 
-        mSmileImages.get(1).setImageLevel(1);
+        SharedPreferences sharedPreferences = DefaultSharedPrefeencesUtil.getDefaultSharedPreferences(this);
+        int continousCheckIn = sharedPreferences.getInt(SharedPreferenceKey.CONITNUOUSE_CHECK_IN, 0);
+        for(int i=0; i<continousCheckIn; i++){
+            mSmileImages.get(i).setImageLevel(1);
+        }
+
+        TextView checkinSummaryTextView = (TextView)findViewById(R.id.checkin_summary);
+        checkinSummaryTextView.setText(String.format("You have checked in continuously for %d day(s).", continousCheckIn));
+
     }
 }
