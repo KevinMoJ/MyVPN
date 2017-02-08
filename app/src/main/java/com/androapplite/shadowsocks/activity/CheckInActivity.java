1beac2b54962aa39f8412f5dd164af47d8f92013
jim <reachjim@gmail.com>
2017-02-08 20:04:18 +0800

    控制笑脸

--- app/src/main/java/com/androapplite/shadowsocks/activity/CheckInActivity.java
+++ app/src/main/java/com/androapplite/shadowsocks/activity/CheckInActivity.java
@@ -1,15 +1,30 @@
 package com.androapplite.shadowsocks.activity;
 
+import android.app.AlarmManager;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.androapplite.shadowsocks.R;
 
+import java.util.ArrayList;
+
 public class CheckInActivity extends AppCompatActivity {
+    private ArrayList<ImageView> mSmileImages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_in);
+        LinearLayout checkin = (LinearLayout)findViewById(R.id.checkin);
+        mSmileImages = new ArrayList<>(checkin.getChildCount());
+        for(int i=0; i<checkin.getChildCount(); i++){
+            LinearLayout linearLayout = (LinearLayout)checkin.getChildAt(i);
+            mSmileImages.add((ImageView)linearLayout.getChildAt(1));
+        }
+
+        mSmileImages.get(1).setImageLevel(1);
     }
 }
