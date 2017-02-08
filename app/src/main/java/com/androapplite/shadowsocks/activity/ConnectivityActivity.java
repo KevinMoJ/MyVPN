fe2a57a918136d1c69f66e14bfab488c6dbaec39
jim <reachjim@gmail.com>
2017-02-09 02:36:25 +0800

    临时保存一下

--- app/src/main/java/com/androapplite/shadowsocks/activity/ConnectivityActivity.java
+++ app/src/main/java/com/androapplite/shadowsocks/activity/ConnectivityActivity.java
@@ -443,9 +443,9 @@ public class ConnectivityActivity extends BaseShadowsocksActivity
         } else if (id == R.id.nav_about) {
             about();
             GAHelper.sendEvent(this, "菜单", "关于");
-//        } else if(id == R.id.nav_settings){
-//            startActivity(new Intent(this, SettingsActivity.class));
-//            GAHelper.sendEvent(this, "菜单", "设置");
+        } else if(id == R.id.nav_settings){
+            startActivity(new Intent(this, SettingsActivity.class));
+            GAHelper.sendEvent(this, "菜单", "设置");
         } else if(id == R.id.nav_buy_vip){
             startActivity(new Intent(this, VIPActivity.class));
             GAHelper.sendEvent(this, "菜单", "VIP");
---
+++ app/src/main/java/com/androapplite/shadowsocks/fragment/AlertSettingsFragment.java
@@ -0,0 +1,30 @@
+package com.androapplite.shadowsocks.fragment;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+
+import com.androapplite.shadowsocks.R;
+import com.androapplite.shadowsocks.preference.DefaultSharedPrefeencesUtil;
+
+/**
+ * Created by jim on 17/2/9.
+ */
+
+public class AlertSettingsFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final PreferenceManager preferenceManager = getPreferenceManager();
+        preferenceManager.setSharedPreferencesName(DefaultSharedPrefeencesUtil.PREFERENCE_NAME);
+        preferenceManager.setSharedPreferencesMode(Context.MODE_PRIVATE);
+        addPreferencesFromResource(R.xml.alert_settings);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            getPreferenceScreen().removePreference(findPreference("app_detect"));
+        }
+    }
+}
