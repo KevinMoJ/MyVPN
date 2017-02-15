b33b9f253566b32ab216297696a19567d53b170a
jim <reachjim@gmail.com>
2017-02-15 11:12:49 +0800

    提炼更新countdown方法

--- app/src/main/java/com/androapplite/shadowsocks/broadcast/WatchVideoADCallbackReceiver.java
+++ app/src/main/java/com/androapplite/shadowsocks/broadcast/WatchVideoADCallbackReceiver.java
@@ -15,10 +15,14 @@ public class WatchVideoADCallbackReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if(intent != null && intent.getAction().equals(Action.VIDEO_AD_FINISH)){
-            SharedPreferences sharedPreferences = DefaultSharedPrefeencesUtil.getDefaultSharedPreferences(context);
-            int countDown = sharedPreferences.getInt(SharedPreferenceKey.TIME_COUNT_DOWN, 0);
-            countDown += 3600;
-            sharedPreferences.edit().putInt(SharedPreferenceKey.TIME_COUNT_DOWN, countDown).commit();
+            increaseCountDown(context);
         }
+    }
+
+    public static void increaseCountDown(Context context) {
+        SharedPreferences sharedPreferences = DefaultSharedPrefeencesUtil.getDefaultSharedPreferences(context);
+        int countDown = sharedPreferences.getInt(SharedPreferenceKey.TIME_COUNT_DOWN, 0);
+        countDown += 3600;
+        sharedPreferences.edit().putInt(SharedPreferenceKey.TIME_COUNT_DOWN, countDown).commit();
     }
 }
