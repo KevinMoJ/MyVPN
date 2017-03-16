cd24f56c7f841579f47cacb348dc850abf152cbd
jim <reachjim@gmail.com>
2017-03-16 20:04:00 +0800

    编译无法通过

--- app/src/main/java/com/androapplite/shadowsocks/GAHelper.java
+++ app/src/main/java/com/androapplite/shadowsocks/GAHelper.java
@@ -1,49 +1,49 @@
 package com.androapplite.shadowsocks;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 /**
  * Created by jim on 16/5/2.
  */
 public final class GAHelper {
-    @NonNull
-    public static final Tracker getTracker(@NonNull Context context){
-        return ((ShadowsocksApplication)context.getApplicationContext()).getTracker();
-    }
-
     public static final void sendEvent(@NonNull Context context,
                                        @NonNull String category,
                                        @NonNull String action,
                                        @NonNull String label,
                                        long value){
-        getTracker(context).send(new HitBuilders.EventBuilder(category, action)
-                .setLabel(label).setValue(value).build());
+        FirebaseAnalytics analytics = FirebaseAnalytics.getInstance(context);
+        Bundle params = new Bundle();
+        params.putString("Action", action);
+        params.putString("Label", label);
+        params.putLong("Value", value);
+        analytics.logEvent(category, params);
     }
 
     public static final void sendEvent(@NonNull Context context,
                                        @NonNull String category,
                                        @NonNull String action,
                                        @NonNull String label){
-        getTracker(context).send(new HitBuilders.EventBuilder(category, action)
-                .setLabel(label).build());
+        FirebaseAnalytics analytics = FirebaseAnalytics.getInstance(context);
+        Bundle params = new Bundle();
+        params.putString("Action", action);
+        params.putString("Label", label);
+        analytics.logEvent(category, params);
     }
 
     public static final void sendEvent(@NonNull Context context,
                                        @NonNull String category,
                                        @NonNull String action){
-        getTracker(context).send(new HitBuilders.EventBuilder(category, action).build());
-    }
-
-    public static final void sendTimingEvent(@NonNull Context context,
-                                             @NonNull String category,
-                                             @NonNull String variable,
-                                             long value){
-        getTracker(context).send(new HitBuilders.TimingBuilder(category, variable, value).build());
+        FirebaseAnalytics analytics = FirebaseAnalytics.getInstance(context);
+        Bundle params = new Bundle();
+        params.putString("Action", action);
+        analytics.logEvent(category, params);
     }
 
     public static final void sendTimingEvent(@NonNull Context context,
@@ -51,14 +51,26 @@ public final class GAHelper {
                                              @NonNull String variable,
                                              long value,
                                              @NonNull String label){
-        getTracker(context).send(new HitBuilders.TimingBuilder(category, variable, value)
-                .setLabel(label)
-                .build());
+        FirebaseAnalytics analytics = FirebaseAnalytics.getInstance(context);
+        Bundle params = new Bundle();
+        params.putString("Variable", variable);
+        params.putLong("Value", value);
+        params.putString("Label", label);
+        analytics.logEvent(category, params);
+    }
+
+    public static final void sendTimingEvent(@NonNull Context context,
+                                             @NonNull String category,
+                                             @NonNull String variable,
+                                             long value){
+        FirebaseAnalytics analytics = FirebaseAnalytics.getInstance(context);
+        Bundle params = new Bundle();
+        params.putString("Variable", variable);
+        params.putLong("Value", value);
+        analytics.logEvent(category, params);
     }
 
     public static final void sendScreenView(@NonNull Context context, @NonNull String screenView){
-        Tracker t = getTracker(context);
-        t.setScreenName(screenView);
-        t.send(new HitBuilders.ScreenViewBuilder().build());
+
     }
 }
