36c2b74106bc1435d48c32b7af98c173cdf7195c
jim <reachjim@gmail.com>
2017-04-14 19:42:04 +0800

    ping超时时间改回3秒

--- app/src/main/java/com/androapplite/shadowsocks/activity/ConnectivityActivity.java
+++ app/src/main/java/com/androapplite/shadowsocks/activity/ConnectivityActivity.java
@@ -1201,7 +1201,7 @@ public class ConnectivityActivity extends BaseShadowsocksActivity
     }
 
     private boolean ping(String ipAddress){
-        int  timeOut =  2000 ;  //超时应该在3钞以上
+        int  timeOut =  3000 ;  //超时应该在3钞以上
         boolean status = false;
         try {
             status = InetAddress.getByName(ipAddress).isReachable(timeOut);     // 当返回值是true时，说明host是可用的，false则不可。
